7c6540b9922e492782fbea5564376bb6a0bd92a7
py20044 <78597548+py20044@users.noreply.github.com>
2021-04-09 23:07:44 +0300

    Update MyAi.java

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,44 +1,35 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.Immutable;
-
-
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.*;
 
+import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Ticket.*;
+
 public class MyAi implements Ai {
-	Board board;
 	GameSetup setup;
 	Player mrX;
-	List<Player> detectives;
-	MyGameStateFactory gameStateFactory;
+	MyGameStateFactory gameStateFactory = new MyGameStateFactory();
 
-	MyAi(Board board, GameSetup setup, Player mrX, List<Player> detectives, MyGameStateFactory gameStateFactory) {
-		this.board = board;
-		this.setup = setup;
-		this.mrX = mrX;
-		this.detectives = detectives;
-		this.gameStateFactory = gameStateFactory;
-	}
 
-	@Nonnull @Override public String name() { return "Name me!"; }
+	@Nonnull @Override public String name() { return "mrX AI"; }
 
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
-		// returns a random move, replace with your own implementation
-		List<Move> moves = new ArrayList<>();
-		List<Integer> scores = new ArrayList<>();
+		this.setup = board.getSetup();
+		Map<Move, Integer> scores = new HashMap<>();
+        var moves = board.getAvailableMoves().asList();
+		ImmutableSet<Player> detectives = makeDetectives(board);
+		Pair<Move, Integer> finalMove = new Pair(moves.get(new Random().nextInt(moves.size())), 0);
+		for(Move move: moves){
 		int score = 0;
-		for (Move move: board.getAvailableMoves()) {
 			if (move.commencedBy().isMrX()) {
 				for (ScotlandYard.Ticket ticket : move.tickets()) {
 					switch (ticket) {
@@ -62,26 +53,49 @@ public class MyAi implements Ai {
 				}
 			});
 
-			for (Player detective : detectives) {
+			/*for (Player detective : detectives) {
 				DijkstraSP dijkstra = new DijkstraSP(setup, detective.location(), detective);
 				score += dijkstra.pathScore(dijkstra.pathTo((int) dest));
-			}
-
-			for(Move newMove : gameStateFactory.build(setup, mrX, ImmutableList.copyOf(detectives)).advance(move).getAvailableMoves()) {
+			}*/
+			
+			//TODO simulate mrX 
+			/*for(Move newMove : gameStateFactory.build(setup, mrX, ImmutableList.copyOf(detectives)).advance(move).getAvailableMoves()) {
 				if(newMove.commencedBy().isMrX()) {
 					score++;
 				}
+			}*/
+			if(score >= finalMove.right()){
+				finalMove = new Pair(move, score);
+			};
+		}
+
+		return finalMove.left();
+	}
+
+
+	public ImmutableSet<Player> makeDetectives(Board board){
+		ArrayList<Player> detectives = new ArrayList<>();
+		for(Piece player : board.getPlayers()){
+			if(player.isDetective() && board.getDetectiveLocation((Piece.Detective)player).isPresent()){
+			ImmutableMap<ScotlandYard.Ticket, Integer> tickets = makeTickets(board.getPlayerTickets(player).get().getCount(TAXI),
+					board.getPlayerTickets(player).get().getCount(BUS),
+					board.getPlayerTickets(player).get().getCount(UNDERGROUND),
+					board.getPlayerTickets(player).get().getCount(DOUBLE),
+					board.getPlayerTickets(player).get().getCount(SECRET));
+
+				detectives.add(new Player(player, tickets, board.getDetectiveLocation((Piece.Detective) player).get()));
 			}
-			moves.add(move);
-			scores.add(score);
 		}
-		Move finalMove = moves.get(0);
-		int index = 0;
+		return ImmutableSet.copyOf(detectives);
+	}
 
-		for(int i = 0; i < moves.size(); i++) {
-			if(scores.get(i) > scores.get(index)) finalMove = moves.get(index);
-		}
-
-		return finalMove;
+	static ImmutableMap<ScotlandYard.Ticket, Integer> makeTickets(
+			int taxi, int bus, int underground, int x2, int secret) {
+		return ImmutableMap.of(
+				TAXI, taxi,
+				BUS, bus,
+				UNDERGROUND, underground,
+				ScotlandYard.Ticket.DOUBLE, x2,
+				ScotlandYard.Ticket.SECRET, secret);
 	}
 }
