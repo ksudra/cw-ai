88210040a7ea5fe36c53126721034502aaa925bb
eo20203 <78903047+eo20203@users.noreply.github.com>
2021-04-14 13:55:36 +0100

    look ahead

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,9 +1,8 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import javax.annotation.Nonnull;
-import javax.annotation.concurrent.Immutable;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -25,6 +24,8 @@ public class MyAi implements Ai {
 		this.setup = board.getSetup();
 		var moves = board.getAvailableMoves().asList();
 		Pair<Move, Integer> finalMove = new Pair(moves.get(new Random().nextInt(moves.size())), 0);
+		Pair<Move, Integer> simulatedMove = new Pair(moves.get(new Random().nextInt(moves.size())), 0);
+		List<List<Node>> adjacencyList = new ArrayList<>();
 
 		for(Piece player : board.getPlayers()){
 			if(player.isMrX()){
@@ -38,8 +39,74 @@ public class MyAi implements Ai {
 			}
 		}
 
+		List<List<Node>> adj = new ArrayList<>();
+		int w = 0;
+		// Initialize list for every node
+		for (int i : setup.graph.nodes()) {
+			List<Node> item = new ArrayList<>();
+			adj.add(item);
+			for (int j : setup.graph.adjacentNodes(i)) {
+				for (ScotlandYard.Transport t : setup.graph.edgeValue(i, j).orElse(null)) {
+					switch (t) {
+						case TAXI: w++;
+						case BUS: w += 2;
+						case UNDERGROUND: w += 4;
+						case FERRY: w = Integer.MAX_VALUE;
+					}
+				}
+				w = w / setup.graph.edgeValue(i, j).orElse(null).size();
+				item.add(new Node(j, w));
+			}
+		}
+
+
 		ImmutableSet<Player> detectives = makeDetectives(board);
 
+		Board.GameState gameSimulation = gameStateFactory.build(setup, mrX, ImmutableList.copyOf(detectives));
+		Board.GameState initial = gameSimulation;
+
+		List<Pair<Move, Integer>> moveList = moveScores(gameSimulation.getAvailableMoves(), ImmutableList.copyOf(detectives), mrX, adj);
+		Move returnedMove = moveList.get(0).left();
+		Stack<Move> moveStack = new Stack<>();
+		for (int i = 2; i > -1; i--) {
+			moveStack.push(moveList.get(i).left());
+		}
+		boolean complete = false;
+		int n = 0;
+		while(!moveStack.isEmpty() && !complete) {
+
+			Move currentMove = moveStack.pop();
+			gameSimulation.advance(currentMove);
+
+			while(gameSimulation.getWinner() != mrX.piece() && n < 5) {
+				if (!gameSimulation.getWinner().isEmpty() && gameSimulation.getWinner() != mrX.piece()) {
+					gameSimulation = initial;
+					break;
+				} else if (gameSimulation.getWinner() == mrX.piece()) {
+					returnedMove = currentMove;
+					complete = true;
+					break;
+				}
+
+				for (Player detective : detectives) {
+					gameSimulation.advance(moveScores(gameSimulation.getAvailableMoves(), List.copyOf(detectives), detective, adj).get(0).left());
+				}
+
+				gameSimulation.advance(moveScores(gameSimulation.getAvailableMoves(), ImmutableList.copyOf(detectives), mrX, adj).get(0).left());
+				n++;
+			}
+
+		}
+		if(n >= 5) {
+			returnedMove = moveList.get(0).left();
+		}
+
+		return returnedMove;
+	}
+
+	List<Pair<Move, Integer>> moveScores(ImmutableSet<Move> moves, List<Player> detectives, Player player, List<List<Node>> adj) {
+		List<Pair<Move, Integer>> moveList = new ArrayList<>();
+		Board.GameState initial = gameStateFactory.build(setup, mrX, ImmutableList.copyOf(detectives));
 		for(Move move: moves){
 			int score = 0;
 			if (move.commencedBy().isMrX()) {
@@ -56,63 +123,88 @@ public class MyAi implements Ai {
 			Object dest = move.visit(new Move.Visitor<Object>() {
 				@Override
 				public Object visit(Move.SingleMove move) {
-					return ((Move.SingleMove) move).destination;
+					return move.destination;
 				}
 
 				@Override
 				public Object visit(Move.DoubleMove move) {
-					return ((Move.DoubleMove) move).destination2;
+					return move.destination2;
 				}
 			});
 
-			List<List<Node>> adj = new ArrayList<>();
 
-			int w = 0;
-
-			// Initialize list for every node
-			for (int i : setup.graph.nodes()) {
-				List<Node> item = new ArrayList<>();
-				adj.add(item);
-				for (int j : setup.graph.adjacentNodes(i)) {
-					for (ScotlandYard.Transport t : setup.graph.edgeValue(i, j).orElse(null)) {
-						switch (t) {
-							case TAXI: w++;
-							case BUS: w += 2;
-							case UNDERGROUND: w += 4;
-							case FERRY: w = Integer.MAX_VALUE;
-						}
-					}
-					w = w / setup.graph.edgeValue(i, j).orElse(null).size();
-					item.add(new Node(j, w));
-
-				}
-			}
-
+			Board.GameState gameState = initial.advance(move);
 			Dijkstra dij = new Dijkstra(setup.graph.nodes().size(), adj);
-			for (Player detective : detectives) {
-				dij.dijkstra(adj, detective.location());
-				score += dij.dist[mrX.location()];
-			}
 
-			/*for (Player detective : detectives) {
-				DijkstraSP dijkstra = new DijkstraSP(setup, detective.location(), detective);
-				score += dijkstra.pathScore(dijkstra.pathTo((int) dest));
-			}*/
-
-
-			for(Move newMove : gameStateFactory.build(setup, mrX, ImmutableList.copyOf(detectives)).advance(move).getAvailableMoves()) {
-				if(newMove.commencedBy().isMrX()) {
-					score++;
+			if(player.piece().isMrX()) {
+				for (Player detective : detectives) {
+					dij.dijkstra(adj, detective.location());
+					score += dij.dist[mrX.location()];
 				}
+				for(Move newMove : gameState.getAvailableMoves()) {
+					if(newMove.commencedBy().isMrX()) {
+						score++;
+					}
+				}
+			} else if(player.piece().isDetective()) {
+				dij.dijkstra(adj, player.location());
+				score = dij.dist[mrX.location()];
 			}
-			if(score >= finalMove.right()){
-				finalMove = new Pair(move, score);
-			}
+			moveList.add(new Pair(move, score));
 		}
-
-		return finalMove.left();
+		Collections.sort(moveList, Comparator.comparing(move -> (int) move.right()));
+		Collections.reverse(moveList);
+		return moveList;
 	}
 
+	final class MyBoard implements Board {
+		private GameState gameState;
+		private MyBoard(GameState gameState) {
+			this.gameState = gameState;
+		}
+
+		@Nonnull
+		@Override
+		public GameSetup getSetup() {
+			return gameState.getSetup();
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Piece> getPlayers() {
+			return gameState.getPlayers();
+		}
+
+		@Nonnull
+		@Override
+		public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			return gameState.getDetectiveLocation(detective);
+		}
+
+		@Nonnull
+		@Override
+		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return gameState.getPlayerTickets(piece);
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableList<LogEntry> getMrXTravelLog() {
+			return gameState.getMrXTravelLog();
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Piece> getWinner() {
+			return gameState.getWinner();
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Move> getAvailableMoves() {
+			return gameState.getAvailableMoves();
+		}
+	}
 
 	public ImmutableSet<Player> makeDetectives(Board board){
 		ArrayList<Player> detectives = new ArrayList<>();
